8e8a82ca91ee6482748e9b3403946b3f66ecf4ce
Oscar Armando Tellez Covarrubias <zaroscar1984@gmail.com>
2021-02-18 17:07:40 -0600

    Estructura para la pagina web con next.js y tailwindcss

---
+++ README.md
@@ -0,0 +1,2 @@
+# MywebNextjs
+Esta es mi pagina web creada con Framework Tailwind y el Framework de React Next.js
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import "tailwindcss/tailwind.css";
+
+function HomePage() {
+  return <div>
+  <button className="text-white hover:text-red-500">
+  Pagina en construccion
+</button>
+
+
+  </div>
+}
+
+export default HomePage
